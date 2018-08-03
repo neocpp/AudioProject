ade0ab9e0b2de77b7b7d01c165956df553a109ba
neocpp <mail.chengbo@gmail.com>
2018-08-03 14:25:56 +0800

    Update MidiView.java
    修改midi等分方式

--- audiokit/src/main/java/com/neo/audiokit/widget/MidiView.java
+++ audiokit/src/main/java/com/neo/audiokit/widget/MidiView.java
@@ -44,7 +44,7 @@ public class MidiView extends View {
     private int inTargetCount;
     private int totalCount;
 
-    private final static int MOD_VALUE = 7;
+    private final static int MOD_VALUE = 12;
 
     public MidiView(Context context) {
         super(context);
